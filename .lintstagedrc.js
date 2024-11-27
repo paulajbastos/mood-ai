7524a157a5fe3f88857cf4a2b2c2a2b194a0a434
Paula Junqueira <paulajbastos@gmail.com>
2024-11-27 13:43:20 -0300

    mood-ai: remove commitlint; update husky

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,13 +1,21 @@
-const path = require("path");
+const path = require('path');
 
 // See https://nextjs.org/docs/pages/building-your-application/configuring/eslint#lint-staged
 const buildEslintCommand = (filenames) =>
   `next lint --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
-    .join(" --file ")}`;
+    .join(' --file ')}`;
 
 module.exports = {
-  "*.{ts,tsx}": [() => "tsc --skipLibCheck --noEmit"],
-  "*.{js,jsx,ts,tsx}": ["prettier --write", buildEslintCommand],
-  "*.{json,md}": ["prettier --write"],
+  '*.{ts,tsx}': [() => 'tsc --skipLibCheck --noEmit'],
+  '*.{js,jsx,ts,tsx}': ['prettier --write', buildEslintCommand],
+  '*.{json,md}': ['prettier --write'],
 };
+
+// module.exports = {
+//   'src/**/*.{js,ts,jsx,tsx}': [
+//     'eslint --cache --cache-location .next/cache/eslint/ --fix',
+//   ],
+//   'src/**/*.{json,css,scss,jsx,tsx,js,ts}': ['yarn format'],
+//   'src/**/*.{ts,tsx}': () => ['npm run typecheck'],
+// };
